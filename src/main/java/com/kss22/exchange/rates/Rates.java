9ce2421c245d8e22484d8944c1dce7f4f79094f5
Karim Safar <kss22@mail.aub.edu>
2023-04-05 00:49:53 +0300

    Phase 4: Calculator

--- src/main/java/com/kss22/exchange/rates/Rates.java
+++ src/main/java/com/kss22/exchange/rates/Rates.java
@@ -20,8 +20,62 @@ public class Rates {
     public TextField lbpTextField;
     public TextField usdTextField;
     public ToggleGroup transactionType;
+    public Label equivalentUsdRateLabel;
+    public Label equivalentLbpRateLabel;
+
+    public void set(){
+        usdTextField.setText("");
+        lbpTextField.setText("");
+    }
     public void initialize() {
         fetchRates();
+    }
+    public void calculator() {
+        String usdAmountString = usdTextField.getText().trim();
+        String lbpAmountString = lbpTextField.getText().trim();
+
+        // Check if the input is valid
+        if (usdAmountString.isEmpty() && lbpAmountString.isEmpty()) {
+            return;
+        }
+
+        // Convert the input to floats
+        float usdAmount = 0;
+        float lbpAmount = 0;
+        if (!usdAmountString.isEmpty()) {
+            try {
+                usdAmount = Float.parseFloat(usdAmountString);
+            } catch (NumberFormatException e) {
+                return;
+            }
+        }
+        if (!lbpAmountString.isEmpty()) {
+            try {
+                lbpAmount = Float.parseFloat(lbpAmountString);
+            } catch (NumberFormatException e) {
+                return;
+            }
+        }
+
+        // Get the exchange rates from the labels
+        float lbpToUsd = 0;
+        float usdToLbp = 0;
+        try {
+            lbpToUsd = Float.parseFloat(sellUsdRateLabel.getText());
+            usdToLbp = Float.parseFloat(buyUsdRateLabel.getText());
+        } catch (NumberFormatException e) {
+            return;
+        }
+
+        // Calculate the equivalent amounts and update the labels
+        if (usdAmount != 0) {
+            float equivalentLbp = usdAmount * usdToLbp;
+            equivalentLbpRateLabel.setText(String.format("%.2f", equivalentLbp));
+        }
+        if (lbpAmount != 0) {
+            float equivalentUsd = lbpAmount / lbpToUsd;
+            equivalentUsdRateLabel.setText(String.format("%.2f", equivalentUsd));
+        }
     }
     private void fetchRates() {
         ExchangeService.exchangeApi().getExchangeRates().enqueue(new Callback<ExchangeRates>() {
